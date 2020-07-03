e413badf278548ca0d06692a45cec704f431bf8a
frederik.steffen <frederik.steffen@gmail.com>
2020-07-03 16:07:17 +0200

    raised mapstruct version that support records

--- client/src/main/java/de/fsteffen/sample/japogrpc/client/web/BookMapper.java
+++ client/src/main/java/de/fsteffen/sample/japogrpc/client/web/BookMapper.java
@@ -4,22 +4,24 @@ import com.google.protobuf.UInt64Value;
 import de.fsteffen.sample.japogrpc.client.web.model.Book;
 import de.fsteffen.sample.japogrpc.grpc.BookProto.BookReply;
 import de.fsteffen.sample.japogrpc.grpc.BookProto.BookRequest;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
 
 import java.util.Optional;
 
 @Mapper
 public interface BookMapper {
 
-    // needed until jdk14 record support for mapstruct is released
-    default BookRequest mapToGrpcModel(Book book) {
-        final BookRequest.Builder bookRequestBuilder = BookRequest.newBuilder();
-        Optional.ofNullable(book.id()).map(UInt64Value::of).ifPresent(bookRequestBuilder::setId);
-        Optional.ofNullable(book.title()).ifPresent(bookRequestBuilder::setTitle);
-        return bookRequestBuilder.build();
+    @BeanMapping(nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+    BookRequest mapToGrpcModel(Book book);
+
+    Book mapFromGrpcModel(BookReply bookReply);
+
+    default UInt64Value mapToUint(Long value) {
+        return Optional.ofNullable(value)
+                       .map(UInt64Value::of)
+                       .orElse(null);
     }
 
-    default Book mapFromGrpcModel(BookReply bookReply) {
-        return new Book(bookReply.getId(), bookReply.getTitle());
-    }
 }
